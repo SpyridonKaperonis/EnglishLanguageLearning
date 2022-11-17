df9803bfe254e1d1db6d4d124b981a26e7b1c08a
SpyridonKaperonis <spyridonkaperonis@gmail.com>
2022-11-17 17:24:32 -0500

    completees implementation of tfidf in data-preprocessing

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -7,17 +7,13 @@ Created on Sun Nov  5 09:58:36 2022
 
 
 import numpy as np
-from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
-from sklearn import datasets
-from sklearn import svm
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import string
-from sklearn.linear_model import LinearRegression
 
 # In case you need to download nltk.
 # import nltk
@@ -26,26 +22,32 @@ from sklearn.linear_model import LinearRegression
 # nltk.download('omw-1.4')
 
 #
-train_dataPath = ('~/Documents/Stevens/MachineLearning_695-A/Final_Project/feedback-prize-english-language-learning/train.csv')
-test_dataPath = ('~/Documents/Stevens/MachineLearning_695-A/Final_Project/feedback-prize-english-language-learning/test.csv')
+train_data_path = ('./feedback-prize-english-language-learning/train.csv')
+test_data_path = ('./feedback-prize-english-language-learning/test.csv')
+
 
 # Loads data into a dataframe.
-def load_data(data):
-    doc = pd.read_csv(data)
-    return doc.copy(True)
+def load_data(path):
+    corpus = pd.read_csv(path)
+    return corpus
 
 def tokenize_data(data):
     tokens = word_tokenize(data)
     return tokens
+
 def lowercase_data(data):
     lowercase = np.char.lower(data)
     return lowercase
+
 def remove_punctuation(data):
     symbols = "!\"#$%&()*+-.,/:;<=>?@[\]^_`{|}~\n"
     no_punctuation = data
     for i in symbols:
         no_punctuation = np.char.replace(no_punctuation, i, ' ')
+        no_punctuation = np.char.replace(no_punctuation, "'", "")        
+        
     return no_punctuation
+
 def numbers_to_words(data):
     newData = []
     for i in data:
@@ -55,10 +57,12 @@ def numbers_to_words(data):
         else:
             newData.append(i)
     return newData
+
 def remove_stop_words(data):
     stop_words = set(stopwords.words('english'))
     words = [w for w in data if not w in stop_words]
     return words
+
 def lemmatize_words(data):
     a = []
     lemmatizer = WordNetLemmatizer()
@@ -70,45 +74,77 @@ def lemmatize_words(data):
 
 # This function removes special characters and stop words.
 # Also, it makes all words lowercase.
-def data_preprocessing(data):
-
+def data_cleanning(data):
     df = data
+    # Loop through data and clean it.
     for i in data['full_text']:
+       
         ndata = tokenize_data(i)
-        ndata = lowercase_data(ndata)
         ndata = remove_punctuation(ndata)
+        ndata = lowercase_data(ndata)
         ndata = numbers_to_words(ndata)
         ndata = remove_stop_words(ndata)
-        ndata = remove_punctuation(ndata)
         ndata = lemmatize_words(ndata)
+        ndata = numbers_to_words(ndata)
         ndata = ' '.join(ndata)
         ndata = remove_punctuation(ndata)
-        df = df.replace(i, ndata)
         
-    return df
+        df = df.replace(i, ndata)
+
+    return pd.DataFrame(df)
+
+def split_test_validation(data):    
+    train_d, val_d = train_test_split(data, test_size=0.2, random_state=1)
+    train_d = pd.DataFrame(train_d)
+    val_d = pd.DataFrame(val_d)
+    return train_d, val_d
     
 def tf_idf(data):
-    tfidfVectorizer = TfidfVectorizer(use_idf=True, input=corpus)
-    tfidf = tfidfVectorizer.fit_transform(data)
-    df = pd.DataFrame(tfidf.toarray())
-    return tfidf
+    
+    vectorizer = TfidfVectorizer(max_features=100,
+                                 min_df=5,
+                                 max_df=0.8,
+                                 ngram_range=(1,3))
+    vectors = vectorizer.fit_transform(data)
+    dense = vectors.todense()
+    denselist = dense.tolist()
+    feature_names = vectorizer.get_feature_names_out()
+    
+    all_keywords = []
+    for i in denselist:
+        x = 0
+        keywords = []
+        for word in i:
+            if word > 0:
+                keywords.append(feature_names[x])
+            x=x+1
+        all_keywords.append(keywords)
+    
+            
+    
+    return vectors, denselist, feature_names, all_keywords
 
-def split_test_validation_byID(data):    
-    train_d, val_d = train_test_split(data, test_size=0.2, random_state=1)
-    return train_d, val_d
 
-
-def model_for_Vocab_pipeline(data):
+def tfidf_pipeline(data):
     dataf = load_data(data)
-    train_data, validation_data = split_test_validation_byID(dataf)
-    df = data_preprocessing(train_data)
-    # X = tf_idf(df['full_text'])
-    # Y = train_data['vocabulary']
-    # print(X)
+
+    train_data, validation_data = split_test_validation(dataf)
+
+    clean_data = data_cleanning(train_data)
+    vectors, denselist, feature_names, all_keywords = tf_idf(clean_data['full_text'])
+    y = train_data[['vocabulary', 'syntax']]
     
-    return df.head()
-    # return tfidf
-    
-# model1_pipeline(train_dataPath)
-x = model_for_Vocab_pipeline(train_dataPath)
-print(x)
+    # Convert vectoried data with keywords into a dataframe
+    data=[]
+    for i in range(len(all_keywords)):
+        data.insert(i, {'words':all_keywords[i],'vectors':denselist[i],})
+    dataframe = pd.DataFrame(data)
+
+    return dataframe, y, clean_data, vectors, denselist, feature_names, all_keywords
+
+
+dataframe, y, clean_data, vectors, denselist, feature_names, all_keywords = tfidf_pipeline(train_data_path)
+print(dataframe)
+dataframe.to_csv('./tfidfData.csv')
+
+
